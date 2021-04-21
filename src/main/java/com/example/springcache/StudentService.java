9ce5e5720c3424251ddfd039bb204ccd2e6562b0
Darshit114 <darshitpatel114@gmail.com>
2021-04-21 09:10:49 +0530

    Initial Commit

---
+++ src/main/java/com/example/springcache/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.springcache;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+	@Cacheable("student")
+	public Student getStudentById(String id) {
+		
+		try {
+			
+			System.out.println("Going to sleep for 5 Secs.. to simulate backend call.");
+            Thread.sleep(1000*5);
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		return new Student(id,"Darshit" ,"V");
+	}
+}
